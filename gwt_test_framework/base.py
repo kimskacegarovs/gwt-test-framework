ff08e79df8e35770ec7220f9f36bedb1b731fc3d
kimskacegarovs <70476754+kimskacegarovs@users.noreply.github.com>
2024-12-29 09:36:42 +0100

    chore(tests) init tests (#6)
    * chore(tests) init tests
    
    * old files cleanup

--- gwt_test_framework/base.py
+++ gwt_test_framework/base.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 
+class GWTMissingDescriptionException(BaseException):
+    pass
+
+
 @dataclass
 class GivenWhenThenDescription:
     test_title: str
@@ -9,11 +13,20 @@ class GivenWhenThenDescription:
     when: str
     then: str
 
+    def render_as_markdown(self) -> str:
+        return f"""
+            ### Test Scenario: {self.test_title}
+
+            **Given**: {self.given}
+
+            **When**: {self.when}
+
+            **Then**: {self.then}
+        """
+
 
 class GivenWhenThenTestScenario(ABC):
-    description_given: str = ""
-    description_when: str = ""
-    description_then: str = ""
+    description: GivenWhenThenDescription
 
     def test(self) -> None:
         self.given()
@@ -34,32 +47,24 @@ class GivenWhenThenTestScenario(ABC):
         raise NotImplementedError(f"{self.__class__.__name__} must implement the 'then' method.")
 
     def validate_descriptions(self) -> None:
-        for field, value in [
-            ("given", self.description_given),
-            ("when", self.description_when),
-            ("then", self.description_then),
-        ]:
-            if not value:
-                raise ValueError(f"{self.__class__.__name__} is missing '{field}' description.")
+        try:
+            description = self.description
+        except AttributeError:
+            raise GWTMissingDescriptionException("Description not provided for the scenario")
 
-    @classmethod
-    def get_description(cls) -> GivenWhenThenDescription:
-        return GivenWhenThenDescription(
-            test_title=cls.__name__, given=cls.description_given, when=cls.description_when, then=cls.description_then
-        )
+        if not isinstance(description, GivenWhenThenDescription):
+            raise GWTMissingDescriptionException(f"Description must be of type {GivenWhenThenDescription.__name__}")
 
     @classmethod
     def get_subclass_descriptions(cls) -> list[GivenWhenThenDescription]:
-        return [sc.get_description() for sc in cls.__subclasses__()]
+        descriptions = []
+        for subclass in cls.__subclasses__():
+            try:
+                descriptions.append(subclass.description)
+            except AttributeError:
+                pass
+        return descriptions
 
     @classmethod
     def render_as_markdown(cls) -> str:
-        return f"""
-            ### Test Scenario: {cls.__name__}
-
-            **Given**: {cls.description_given}
-
-            **When**: {cls.description_when}
-
-            **Then**: {cls.description_then}
-        """
+        return cls.description.render_as_markdown()
